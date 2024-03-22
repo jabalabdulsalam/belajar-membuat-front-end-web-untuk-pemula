07aa109421afcb949176dfac1731791815e08802
Jabal Abdul Salam <jabalabdulsalam@gmail.com>
2024-03-23 02:39:13 +0700

    sudah menyelesaikan input event

---
+++ scripts/index-2.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
+  document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+
+  // Input Event - onInput
+  document.getElementById("inputNama").addEventListener("input", function () {
+    const jumlahKarakterDiketik =
+      document.getElementById("inputNama").value.length;
+    const jumlahKaraterMaksimal =
+      document.getElementById("inputNama").maxLength;
+
+    console.log("jumlahKarakterDiketik: ", jumlahKarakterDiketik);
+    console.log("jumlahKarakterMaksimal: ", jumlahKaraterMaksimal);
+    const sisaKarakterUpdate = jumlahKaraterMaksimal - jumlahKarakterDiketik;
+    document.getElementById("sisaKarakter").innerText =
+      sisaKarakterUpdate.toString();
+
+    if (sisaKarakterUpdate === 0) {
+      document.getElementById("sisaKarakter").innerText =
+        "Batas maksimal tercapai!";
+    } else if (sisaKarakterUpdate <= 5) {
+      document.getElementById("notifikasiSisaKarakter").style.color = "red";
+    } else {
+      document.getElementById("notifikasiSisaKarakter").style.color = "black";
+    }
+  });
+
+  // Input Event - onFocus
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
+  });
+
+  // Input Event - onBlur
+  document.getElementById("inputNama").addEventListener("blur", function () {
+    console.log("inputNama: blur");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "hidden";
+  });
+
+  // Input Event - onChange
+  document
+    .getElementById("inputCaptcha")
+    .addEventListener("change", function () {
+      console.log("inputCaptcha: change");
+
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+      const submitButtonStatus = document.getElementById("submitButton");
+
+      if (inputCaptcha === "PRNU") {
+        submitButtonStatus.removeAttribute("disabled");
+      } else {
+        submitButtonStatus.setAttribute("disabled", "");
+      }
+    });
+
+  document
+    .getElementById("formDataDiri")
+    .addEventListener("submit", function (event) {
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+
+      if (inputCaptcha === "PRNU") {
+        alert("Selamat! Captcha anda lolos!");
+      } else {
+        alert("Captcha anda belum tepat!");
+        document.getElementById("submitButton").setAttribute("disabled", "");
+      }
+      event.preventDefault();
+    });
+
+  // Input Event - onCopy
+  document.getElementById("inputCopy").addEventListener("copy", function () {
+    alert("Anda telah men-copy sesuatu...");
+  });
+
+  // Input Event - onPaste
+  document.getElementById("inputPaste").addEventListener("paste", function () {
+    alert("Anda telah mem-paste sebuah teks...");
+  });
+});
